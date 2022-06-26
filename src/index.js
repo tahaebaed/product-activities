7412c44430ffe90059f3d83c568dfc37852afa70
Taha Ebaed <tahaebaed2@gmail.com>
2022-06-26 22:45:40 +0200

    3-products_activities implment providers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './App'
+import store from './store'
+
+import './index.css'
+
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+        <ReactQueryDevtools initialIsOpen='false' position='bottom-right' />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
