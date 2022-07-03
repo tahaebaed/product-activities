93f6faffab5e5078fdb89ee1fef997dedfcfea5b
Taha Ebaed <tahaebaed2@gmail.com>
2022-07-03 13:19:45 +0200

    update user logged state handling with out props to autroutes

--- src/routes/NoAuthRoutes.js
+++ src/routes/NoAuthRoutes.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 
-function NoAuthRoutes({ user, children, redirectTo }) {
+function NoAuthRoutes({ children, redirectTo }) {
+  const user = useSelector(state => state.user)
+
   if (!user) {
     return <Navigate to={redirectTo} replace />
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,66 +12,62 @@ import {
 import Loading from '../utilities/Loading'
 import ProductInfo from '../pages/ProductInfo'
 import NoAuthRoutes from './NoAuthRoutes'
-import { useSelector } from 'react-redux'
 import AuthRoutes from './AuthRoutes'
 
-const RenderRoutes = () => {
-  const user = useSelector(state => state.user)
-  return (
-    <React.Suspense fallback={<Loading />}>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <AuthRoutes redirectTo='/user/products' user={user}>
-              <Login />
-            </AuthRoutes>
-          }
-        />
-        <Route
-          path='/Sign%20up'
-          element={
-            <AuthRoutes redirectTo='/user/products' user={user}>
-              <Signup />
-            </AuthRoutes>
-          }
-        />
-        <Route
-          path='/user/profile'
-          element={
-            <NoAuthRoutes redirectTo='/' user={user}>
-              <Profile />
-            </NoAuthRoutes>
-          }
-        />
-        <Route
-          path='/user/products'
-          element={
-            <NoAuthRoutes redirectTo='/' user={user}>
-              <ProductList />
-            </NoAuthRoutes>
-          }
-        />
-        <Route
-          path='/user/edit'
-          element={
-            <NoAuthRoutes redirectTo='/' user={user}>
-              <EditProfile />
-            </NoAuthRoutes>
-          }
-        />
-        <Route
-          path='/product/:id'
-          element={
-            <NoAuthRoutes redirectTo='/' user={user}>
-              <ProductInfo />
-            </NoAuthRoutes>
-          }
-        />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </React.Suspense>
-  )
-}
+const RenderRoutes = () => (
+  <React.Suspense fallback={<Loading />}>
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <AuthRoutes redirectTo='/user/products'>
+            <Login />
+          </AuthRoutes>
+        }
+      />
+      <Route
+        path='/Sign%20up'
+        element={
+          <AuthRoutes redirectTo='/user/products'>
+            <Signup />
+          </AuthRoutes>
+        }
+      />
+      <Route
+        path='/user/profile'
+        element={
+          <NoAuthRoutes redirectTo='/'>
+            <Profile />
+          </NoAuthRoutes>
+        }
+      />
+      <Route
+        path='/user/products'
+        element={
+          <NoAuthRoutes redirectTo='/'>
+            <ProductList />
+          </NoAuthRoutes>
+        }
+      />
+      <Route
+        path='/user/edit'
+        element={
+          <NoAuthRoutes redirectTo='/'>
+            <EditProfile />
+          </NoAuthRoutes>
+        }
+      />
+      <Route
+        path='/product/:id'
+        element={
+          <NoAuthRoutes redirectTo='/'>
+            <ProductInfo />
+          </NoAuthRoutes>
+        }
+      />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </React.Suspense>
+)
 
 export default RenderRoutes
